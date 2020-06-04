f9d6f8876e3c955a17aab190a8c22865e08d6383
wazid <wazidshh@gmail.com>
2020-06-04 15:04:32 +0530

    fix:version fixed

--- karnataka_covid.php
+++ karnataka_covid.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Karnataka COVID 19 tracker
  * Plugin URI:        https://grandworks.co
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0
  * Author:            GrandWorks
  * Author URI:        https://grandworks.co
  * License:           GPL-2.0+
